47b4b4e719f59ac7d3cb5f53d06eca9921fd15c6
Mariso <marietoursduplouich@gmail.com>
2021-10-01 17:40:40 +0200

    MAJ

--- src/App.js
+++ src/App.js
@@ -20,9 +20,63 @@ const App = () => {
   const [message, setMessage] = useState("");
   
 
+const isEmail = () => {
+
+  let mail = document.getElementById('not-mail');
+  let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (email.match(regex)){
+
+    mail.style.display='none';
+    return true;
+    
+  } else {
+    mail.style.display ='block';
+    mail.style.animation ='dongle 1s';
+    setTimeout(()=>{
+      mail.style.animation ='none';
+    },1000);
+    return false;
+  }
+}
+
+const failMessage = (message) => {
+
+  let formMess = document.querySelector('.form-message');
+  formMess.innerHTML = message;
+  formMess.style.opacity = '1';
+  formMess.style.background = 'rgb(253,87,87)';
+
+  document.getElementById('name').classList.add('error');
+  document.getElementById('firstname').classList.add('error');
+  document.getElementById('email').classList.add('error');
+  document.getElementById('message').classList.add('error');
+}
+
+
+const successMessage = () =>{
+  let formMess = document.querySelector ('.form-message');
+  formMess.innerHTML = 'Message envoyé ! Nous vous recontacterons dès que possible.';
+  formMess.style.background = '00C1ec';
+  formMess.style.opacity = '1';
+  document.getElementById('name').classList.remove('error');
+  document.getElementById('firstname').classList.remove('error');
+  document.getElementById('email').classList.remove('error');
+  document.getElementById('message').classList.remove('error');
+
+setTimeout(() => {
+formMess.style.opacity = '0';
+}, 5000);
+
+
+}
+
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    if(name && firstname && isEmail() && message)
     sendFeedback("marieform", {
       name,
       firstname,
@@ -30,6 +84,9 @@ const App = () => {
       email,
       message,
     });
+   else {
+    failMessage("Merci de remplir correctement les champs requis*");
+  }
   };
 
   const sendFeedback = (templateId, variables) => {
@@ -37,7 +94,7 @@ const App = () => {
     window.emailjs
       .send("Marie", templateId, variables)
       .then((res) => {
-        console.log('success !');
+        successMessage();
         setName("");
         setFirstName("");
         setPhone("");
@@ -45,9 +102,11 @@ const App = () => {
         setMessage("");
       })
       .catch(
-        (err) =>
-          document.querySelector('.form-message').innerHTML =
-            "Une erreur s'est produite, veuillez réessayer.")
+        (err) =>{
+
+          failMessage("Une erreur s'est produite, veuillez réessayer.")
+         }
+          );            
   };
 
   return (
@@ -69,7 +128,7 @@ const App = () => {
           id="firstname"
           name="firstname"
           onChange={(e) => setFirstName(e.target.value)}
-          placeholder="firstname *"
+          placeholder="prénom *"
           value={firstname}
           autoComplete="off"
         />
